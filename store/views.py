4b9593c9bd53c138a80eccacbba7b7c869233e21
samidevel <yesideveloper@gmail.com>
2024-06-18 20:24:03 -0500

    se termina paginacion funcional se modifica el archivo store, se crea navegacion correcta en la navegacion

--- store/views.py
+++ store/views.py
@@ -12,10 +12,12 @@ def store(request, category_slug=None):
     products = None
     
     if category_slug != None:
-        categories = get_object_or_404(Category, slug=category_slug)    
+        categories = get_object_or_404(Category, slug=category_slug)   
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 5)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count =  products.count()
-        
     else:
         products = Product.objects.all().filter(is_available=True)
         paginator = Paginator(products, 5)
@@ -30,6 +32,8 @@ def store(request, category_slug=None):
         'product_count':product_count,
     }
     return render(request, 'store/store.html', context)
+
+
 
 
 def product_detail(request, category_slug, product_slug):
